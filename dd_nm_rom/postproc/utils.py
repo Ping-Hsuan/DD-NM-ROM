cc3dff9a64b655ba211baf30e6c97d1fa3384f33
Ping-Hsuan <pht2@illinois.edu>
2025-04-29 12:39:58 -0400

    update

--- dd_nm_rom/postproc/utils.py
+++ dd_nm_rom/postproc/utils.py
@@ -5,7 +5,8 @@ def set_style(pyplot):
     "text.usetex": False,
     "font.size": 20,
     "font.family": "serif",
-    "font.serif": "Computer Modern",
+#   "font.serif": "Computer Modern",
+    "font.serif": "Dejavu Serif",
     "figure.max_open_warning": int(1e3)
   })
   return pyplot
