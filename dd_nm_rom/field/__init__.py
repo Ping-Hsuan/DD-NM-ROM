17c98b1a5571671751a54542567177aff175ae8b
Ping-Hsuan <pht2@illinois.edu>
2025-07-29 23:12:22 -0400

    support different initial condition for u and v

--- dd_nm_rom/field/__init__.py
+++ dd_nm_rom/field/__init__.py
@@ -2,14 +2,16 @@ __all__ = [
   "Burgers2DExact",
   "SinMultiPeak",
   "SinPeak",
-  "PoissonForce"
+  "PoissonForce",
+  "MultiPeak"
 ]
 
 from .exact import Burgers2DExact
 from .sin_multi_peak import SinMultiPeak
 from .sin_peak import SinPeak
 from .poisson_force import PoissonForce
+from .multi_peak import MultiPeak
 
 # Data types
 from typing import Union
-FIELD_TYPES = Union[Burgers2DExact, SinMultiPeak, SinPeak, PoissonForce]
+FIELD_TYPES = Union[Burgers2DExact, SinMultiPeak, SinPeak, PoissonForce, MultiPeak]
